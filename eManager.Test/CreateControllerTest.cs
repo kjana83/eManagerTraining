8ff74ae185a966754555de181e1e41bca73acd56
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2014-07-24 06:33:36 +0530

    Initial Commit

--- eManager.Test/CreateControllerTest.cs
+++ eManager.Test/CreateControllerTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Web.Mvc;
+using eManager.Web.Controllers;
+using eManager.Web.Infrastructure;
+using eManager.Web.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace eManager.Test
@@ -10,9 +13,10 @@ namespace eManager.Test
         [TestMethod]
         public void CreateEmployeeTest()
         {
-            //var controller = new EmployeeController();
-            //var result = controller.Create(1) as ViewResult;
-            //Assert.AreEqual(result.Model.Employye);
+            var controller = new EmployeeController(new DepartmentDb());
+            var result = controller.Create(1) as ViewResult;
+            var model = result.Model as CreateEmployeeViewModel;
+            Assert.AreEqual(1,model.DepartmentId);
         }
     }
 }
